d792547c5b7b5bcb10d21798ff0d90466afd830b
The Cloud Sandbox <thecloudsandbox@outlook.com>
2022-07-15 22:50:12 -0400

    more updates

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,44 +1,45 @@
+board = ['0','1','2','3','4','5','6','7','8','9']
+
 def display_board(board):
-    #Using pa to represent play area here
-    pa = [' ','1','2','3','4','5','6','7','8','9']
+
     print('\n')
     print('\t     |     |')
-    print('\t   {} |  {}  |  {} '.format(pa[1],pa[2],pa[3]))
+    print('\t   {} |  {}  |  {} '.format(board[1],board[2],board[3]))
     print('\t_____|_____|_____')
     print('\t     |     |')
-    print('\t   {} |  {}  |  {} '.format(pa[4],pa[5],pa[6]))
+    print('\t   {} |  {}  |  {} '.format(board[4],board[5],board[6]))
     print('\t_____|_____|_____')
     print('\t     |     |')
-    print('\t   {} |  {}  |  {} '.format(pa[7],pa[8],pa[9]))
+    print('\t   {} |  {}  |  {} '.format(board[7],board[8],board[9]))
     print('\t     |     |')
+   
+
+   
 
 def player_input():
     #List to hold playing tokens to choose from
     token_choice = ['X','O']
+    marker = ''
     
-    #List of values to use for input validaton
-    input_validation_list = [1,2]
-    selection = 0
-    input_valid_range = range(3)
     #Input validation
-    while selection not in input_validation_list and input_valid_range:
+    while marker not in token_choice:
         #Error handling for input
-        player_input = input('Please enter 1 for "X" or 2 for "O": ')
+        player_input = input('Please enter 1 for "X" or 2 for "O": ').upper()
         try:
-            selection = int(player_input)
+            marker = int(player_input)
         except ValueError:
             print('You entered an invalid character: ',player_input)
         
         #Assigning tokens to players
-        if selection == 1:
-            player1 = token_choice[0]
-            if player1 == token_choice[0]:
-                player2 = token_choice[1]
-        elif selection == 2:
-            player1 = token_choice[1]
-            if player1 == token_choice[1]:
-                player2 = token_choice[0]
-                
-    print(player1,player2)
-
-player_input()
+        if marker == 1:
+            return ('X','O')
+        else:
+            return ('O','X')
+        
+def place_marker(board,marker,position):
+    board[position] = marker
+    
+    
+place_marker(board,'X',5)
+print(display_board(board))
+#player_input()
